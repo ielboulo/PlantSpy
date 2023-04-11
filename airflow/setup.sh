d3a50b919cc873c7a0ad35453980bc7615c0881d
wromy <89146957+wromy@users.noreply.github.com>
2023-04-11 20:44:11 +0400

    Update setup.sh

--- airflow/setup.sh
+++ airflow/setup.sh
@@ -6,9 +6,8 @@ echo -e "AIRFLOW_UID=$(id -u)\nAIRFLOW_GID=0" > .env
 
 docker-compose up airflow-init
 
-#placer le fichier test_api_dag.py et le dossier Images dans le dossier ./dag
-mv Image ./dags
-cp test_api_dag.py ./dags
+#placer le fichier test_api_dag.py 
+cp airflow_pipeline_plantspy.py ./dags
 
 #lancer le service airflow
 docker-compose up -d
