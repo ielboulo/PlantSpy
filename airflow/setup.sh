4668665d01399daa3db64337a22ad68e49e9a222
ielboulo <46560616+ielboulo@users.noreply.github.com>
2023-04-18 09:58:51 +0200

    Update setup.sh

--- airflow/setup.sh
+++ airflow/setup.sh
@@ -7,7 +7,7 @@ echo -e "AIRFLOW_UID=$(id -u)\nAIRFLOW_GID=0" > .env
 docker-compose up airflow-init
 
 #placer le fichier test_api_dag.py 
-cp airflow_pipeline_plantspy.py ./dags
+cp mlops_plantspy.py ./dags
 
 #lancer le service airflow
 docker-compose up -d
